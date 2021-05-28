3a42fec51e35fdb0ef8b9532c60aec6eaeb2f3de
Praise <epraise17@yahoo.com>
2021-05-27 23:57:21 -0400

    started crud operations on firestore db

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# This is a useless app for me to learn and play around with react!!
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Category.js
+++ src/components/Category.js
@@ -3,37 +3,72 @@ import { db } from "../firebase";
 
 const Category = () => {
     const [categoryName, setCategoryName] = useState("");
-
-    const [loader, setLoader] = useState(false);
+    const [categoryList, setCategoryList] = useState([]);
+    const [categoryExists, setCategoryExists] = useState(false);
 
     const handleSubmitCategoryForm = (e) => {
         e.preventDefault();
-        setLoader(true);
-        // TODO: validation on the input data? when data is empty? when data already exists?
 
-        db.collection('categories').add({
-            name:categoryName
-        })
-        .then(() => {
-            alert('Category entry successful');
-            setLoader(false);
-        })
-        .catch((error) => {
-            alert(error.message);
-            setLoader(false);
-        });
+        if (categoryName.length <= 0) {
+            alert("empty category, please enter something in category field");
+            return;
+        }
 
-        setCategoryName("");
+        setCategoryList(fetchCategories());
+        const temp = categoryList.find(element => element["name"] == categoryName);
+        setCategoryExists(temp != undefined);
+        
+        if (!categoryExists) {
+            db.collection('categories').add({
+                name:categoryName
+            })
+            .then(() => {
+                alert('Category entry successful');
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+
+            setCategoryName("");
+        } else {
+            alert('Category exists in db already.!');
+        }
     };
 
+    const fetchCategories = (e) => {
+        const checkCategoryName = [];
+        db.collection('categories')
+            .get()
+            .then(snapshot => {
+                // console.log(snapshot);
+                snapshot.forEach(doc => {
+                    const data = doc.data();
+                    checkCategoryName.push(data);
+                });
+            })
+            .catch (error => console.log(error));
+        
+        return checkCategoryName;
+    }
+
+    // debug purposes
+    const printCategories = (e) => {
+        e.preventDefault();
+        setCategoryList(fetchCategories());
+        categoryList.forEach(category => {
+            console.log(category["name"]);
+        });
+    }
+
     return (
-        <form className="form" onSubmit={handleSubmitCategoryForm}>
+        <form className="form">
             <h1>Create A Category</h1>
 
             <label>Category Name </label>
             <input placeholder="category name" value={categoryName} onChange={(e) => setCategoryName(e.target.value)} />
 
-            <button type="submit" style={{background : loader ? "#ccc": "rgb(239, 239, 239)"}}>Submit</button>
+            <button onClick={handleSubmitCategoryForm}>Submit</button>
+            <button onClick={printCategories}>print categories to console</button>
         </form>
     )
 }
